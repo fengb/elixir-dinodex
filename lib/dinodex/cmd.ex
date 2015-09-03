8b0cb3cf7d136c6afff6303a0f961be34afeab96
fengb <contact@fengb.info>
2015-09-03 17:30:34 -0500

    Compiling code!

--- lib/dinodex/cmd.ex
+++ lib/dinodex/cmd.ex
@@ -1,45 +1,91 @@
 defmodule Dinodex.Cmd do
-  def load(filename) do
-    @all_dex = (File.stream!(filename) |> Dinodex.File.load) ++ @all_dex
+  use GenServer
+
+  def init(dex \\ []) do
+    state = %{
+      dex: dex,
+      filters: [],
+    }
+
+    {:ok, state}
   end
 
-  def filter(command) do
-    @filters << command
-  end
-
-  def clear do
-    @filters = []
-  end
-
-  def print do
-    Enum.each filtered_dex, fn(dino) ->
-      print(dino)
-      IO.puts("-----")
+  @calls %{
+    "load"   => :load,
+    "filter" => :filter,
+    "clear"  => :clear,
+    "print"  => :print,
+  }
+  def handle_call(cmd_line, _from, state) do
+    [cmd_string | args] = String.split(cmd_line)
+    cmd = @calls[cmd_string]
+    if cmd do
+      apply(Dinodex.Cmd, cmd, [state] ++ args)
+    else
+      {:reply, nil, state}
     end
   end
 
-  def print(name) when is_binary(name) do
+  def load(state, filename) do
+    new_dinos = File.stream!(filename) |> Dinodex.File.load
+    new_state = %{
+      dex: new_dinos ++ state[:dex],
+      filters: state[:filters],
+    }
+
+    {:reply, nil, new_state}
+  end
+
+  def filter(state, command) do
+    new_filter = nil
+    new_state = %{
+      dex: state[:dex],
+      filters: [] ++ state[:filters],
+    }
+
+    {:reply, nil, new_state}
+  end
+
+  def clear(state) do
+    new_state = %{
+      dex: state[:dex],
+      filters: [],
+    }
+
+    {:reply, nil, new_state}
+  end
+
+  def print(state) do
+    output = filtered_dex(state)
+             |> Enum.map(&serialize/1)
+             |> Enum.join("\n-----\n")
+    {:reply, output, state}
+  end
+
+  def print(state, name) do
     regex = Regex.compile(name)
-    dino = Enum.find(filtered_dex, fn(dino) -> Regex.match? regex, dino[:name] end)
-    cond do
+    dino = filtered_dex(state)
+           |> Enum.find(fn(dino) -> Regex.match? regex, dino[:name] end)
+    output = cond do
       dino ->
-        print(dino)
-      List.first(@filters) ->
-        IO.puts "Dino #{name} not found. Maybe it's filtered out?"
+        serialize(dino)
+      List.first(state[:filters]) ->
+        "Dino #{name} not found. Maybe it's filtered out?"
       true ->
-        IO.puts "Dino #{name} not found."
+        "Dino #{name} not found."
     end
+
+    {:reply, output, state}
   end
 
-  defp print(dino) do
-    Enum.each dino, fn({key, value}) ->
-      if value != nil do
-        IO.puts("#{key}: #{value}
-      end
-    end
+  defp serialize(dino) do
+    dino
+    |> Enum.filter(fn({_key, value}) -> value end)
+    |> Enum.map(fn({key, value}) -> "#{key}: #{value}" end)
+    |> Enum.join("\n")
   end
 
-  defp filtered_dex, do: run_filter(@all_dex, @filters)
+  defp filtered_dex(state), do: run_filter(state[:dex], state[:filters])
 
   defp run_filter(dex, []), do: dex
   defp run_filter(dex, [filter | tail]) do
