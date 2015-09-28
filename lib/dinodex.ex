fbcc38d38562d6b9d174f4835a4c384a936fa1fe
fengb <contact@fengb.info>
2015-09-28 18:04:50 -0500

    Added hack to get "mix run" to work

--- lib/dinodex.ex
+++ lib/dinodex.ex
@@ -1,4 +1,11 @@
 defmodule Dinodex do
+  use Application
+
+  def start(_type, args) do
+    # TODO: add a real supervisor
+    main(args)
+  end
+
   def main(_args) do
     Dinodex.Cmd.repl
     IO.puts "Goodbye!"
--- mix.exs
+++ mix.exs
@@ -15,7 +15,8 @@ defmodule Dinodex.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [mod: {Dinodex, []},
+     applications: [:logger]]
   end
 
   # Dependencies can be Hex packages:
